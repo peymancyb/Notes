a733aa17725a637289572deb99d627eed3b674e0
peymancyb <paymanghazvini@yahoo.com>
2017-12-09 23:06:03 +0400

    Note 2 Added

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,4 +1,6 @@
-// This Lesson is about what is exactly means when we use `${}` in javascript
+//==================================Note 1===========================================
+
+// This Note is about what is exactly means when we use `${}` in javascript
 // when we use `${}` in javascript means that we can include all javascript
 // expressions such as variables , functions ...
 // lets consider following example:
@@ -19,3 +21,17 @@ console.log(typeof callPersonSimple +" "+ typeof callPerson);
 // well actually its possible to use this way to peresent variables, but with the
 // first method you can pass any JavaScript expressions , while in the last method
 // you can only pass variables.
+
+
+//==================================Note 2===========================================
+
+// This Note is about explaining what exactly ... do in JavaScript.
+// 3 dots(...) lets you to copy the object or an array to use the copy of the object or
+// an array rather than modifying the orginal object.
+// basically ... is doing something like slice() or assign() functions in JavaScript.
+// Lets conside following example:
+
+const aObject = [1, 2, 3, 4];
+const copeid = [...aObject];
+console.log(copeid);
+// now we have the copy of the array and we can modify the new array
